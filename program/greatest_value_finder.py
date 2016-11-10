f8ff57dd18f1adff745324bcb681076fbe721c3a
Matt <matthew.lourenco@ocsbstudent.ca>
2016-11-10 12:35:09 -0500

    Undid hardcoding and made it more random

--- program/greatest_value_finder.py
+++ program/greatest_value_finder.py
@@ -3,6 +3,7 @@
 # This is a program that finds the largest value in an array.
 
 from copy import deepcopy
+from numpy import random
 
 def find_greatest(array):
     greatest_value = float(0)
@@ -11,5 +12,8 @@ def find_greatest(array):
             greatest_value = index
     return greatest_value
 
-my_array = [13, 17, 5, 23, 9]
+my_array = []
+for random_generation in range(0, random.randint(1, 11)):
+    my_array.append(random.randint(1, 26))
+print(my_array)
 print(find_greatest(my_array))
